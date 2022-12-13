1ad3ff7c362af5119c0f67783073604200351b01
quiet-ocean <91833579+quiet-ocean@users.noreply.github.com>
2022-12-13 09:47:21 +0200

    hard code post request content type

--- src/features/pages/activate-user/activate-user.page.tsx
+++ src/features/pages/activate-user/activate-user.page.tsx
@@ -8,12 +8,14 @@ import { ActivateUserAction } from '../../../common/state/auth/auth.actions';
 import { Activation } from '../../../swagger2Ts/interfaces';
 import { IServices } from '../../../common/services/initiate';
 import { ServicesContext } from '../../../common/contexts';
+import { RouteChildrenProps } from 'react-router-dom';
+import { ROUTES } from '../../../common/constants';
 
 export interface VerifyUserPageProps {
   activateUser: (args: Activation) => Promise<void>
 }
 
-const ActivateUserPage: React.FC<VerifyUserPageProps> = (props) => {
+const ActivateUserPage: React.FC<RouteChildrenProps & VerifyUserPageProps> = (props) => {
   const tokens = window.location.pathname.split('/activate')[1];
   const [uid, token] = tokens.split('/').filter((t) => t);
 
@@ -23,9 +25,11 @@ const ActivateUserPage: React.FC<VerifyUserPageProps> = (props) => {
 
   const activate = async () => {
     if (uid && token) {
+      services.loading.actions.start();
       try {
         await props.activateUser({ uid, token });
         services.snackbar.actions.open({ content: 'User Activated Successfuly' });
+        props.history.push(ROUTES.login);
       } catch (e: any) {
         services.snackbar.actions.open({ content: e.detail, type: 'error' });
       } finally {
@@ -39,7 +43,7 @@ const ActivateUserPage: React.FC<VerifyUserPageProps> = (props) => {
   }, [])
 
   return (
-    <StyledContainer>activation</StyledContainer>
+    <StyledContainer></StyledContainer>
   );
 }
 
