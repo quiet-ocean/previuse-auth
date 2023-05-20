354eafb9e3166de6112d51cb7293df1ddf86f06b
דניאל בן זקן <benzakends@gmail.com>
2023-05-20 06:26:11 +0000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const host = 'http://localhost:8000';
-// const host = 'http://3.125.137.91:8000';
+// const host = 'http://localhost:8000';
+const host = 'http://3.125.137.91:8000';
 
 module.exports = function(app) {
   app.use(createProxyMiddleware(['/auth', '/social-auth'], {
